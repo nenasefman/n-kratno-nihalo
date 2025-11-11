d865848a164ba8430f568c78ad62316972e01f59
Nena Šefman Hodnik <nena.sefman@gmail.com>
2025-11-11 15:39:49 +0100

    popravljeno dvojno nihalo

--- koda/dvojno_nihalo.py
+++ koda/dvojno_nihalo.py
@@ -3,17 +3,6 @@ import sympy as sp
 from scipy.integrate import odeint
 
 
-## -- PODATKI --
-
-# dolžine vrvic in mase krogljic
-l1, l2 = 1, 1
-m1, m2 = 1, 1
-# gravitacija
-g = 9.81
-
-
-
-
 ## -- SIMBOLIČNO IZRAČUNAMO SISTEM DIFERENCIALNIH ENAČB --
 
 t = sp.Symbol('t', real=True)
@@ -60,27 +49,59 @@ ddtheta1 = sp.Derivative(theta1, (t, 2))
 ddtheta2 = sp.Derivative(theta2, (t, 2))
 
 # pretvoriva v sistem diferencialnih enačb 1. reda
-z1, z2 = dtheta1, dtheta2
-dz1, dz2 = sp.solve([eq1, eq2], [ddtheta1, ddtheta2], simplify=True, rational=False).values()
+# z1, z2 = dtheta1, dtheta2
+dz1_o, dz2_o = sp.solve([eq1, eq2], [ddtheta1, ddtheta2], simplify=True, rational=False).values()
 
 
 ## -- REŠIMO SISTEM DIFERENCIALNIH ENAČB --
 
-#pretvorimo nazaj v numerične funkcije 
-f_dz1 = sp.lambdify([theta1, theta2, z1, z2, l1, l2, m1, m2, g], dz1, "numpy")
-f_dz2 = sp.lambdify([theta1, theta2, z1, z2, l1, l2, m1, m2, g], dz2, "numpy")
+#zamenjava funkcij s simboli
+th1, th2, z1, z2 = sp.symbols("th1 th2 z1 z2")
+
+#slovar za menjavo funkcij in odvodov
+zamenjave_sl = {
+    theta1: th1,
+    theta2: th2,
+    sp.Derivative(theta1, t): z1,
+    sp.Derivative(theta2, t): z2
+}
+
+# naredimo zamenjavo v dobljenih izrazih za dz1 in dz2
+dz1 = sp.simplify(dz1_o.subs(zamenjave_sl))
+dz2 = sp.simplify(dz2_o.subs(zamenjave_sl))
+
+# iz simbolnega računanja gremo nazaj na numerično računanje
+f_dz1 = sp.lambdify([th1, th2, z1, z2, l1, l2, m1, m2, g], dz1, "numpy")
+f_dz2 = sp.lambdify([th1, th2, z1, z2, l1, l2, m1, m2, g], dz2, "numpy")
+
+rez = f_dz1(np.pi/4, np.pi/3, 0.1, 0.2, 1, 1, 1, 1, 9.81)
+print("Rezultat f_dz1 =", rez)
+print("Tip rezultata =", type(rez))
+
+# sistem diferencialnih enačb
+def sistem_num(y, t, l1, l2, m1, m2, g):
+    """
+    y = [theta1, z1, theta2, z2]
+    vrne odvodni vektor: [dtheta1/dt, dz1/dt, dtheta2/dt, dz2/dt]
+    """
+    theta1, z1, theta2, z2 = y
+    dz1 = f_dz1(theta1, theta2, z1, z2, l1, l2, m1, m2, g)
+    dz2 = f_dz2(theta1, theta2, z1, z2, l1, l2, m1, m2, g)
+    return z1, dz1, z2, dz2
+
+# dolžine vrvic in mase krogljic
+l1, l2 = 1, 1
+m1, m2 = 1, 1
+# gravitacija
+g = 9.81
 
 
-#def sistem_num(y, l1, l2, m1, m2, g):
-#    theta1, z1, theta2, z2 = y
-#    dz1_vred = f_dz1(theta1, theta2, z1, z2, l1, l2, m1, m2, g)
-#    dz2_vred = f_dz2(theta1, theta2, z1, z2, l1, l2, m1, m2, g)
-#    return [z1, dz1_vred, z2, dz2_vred]
-#
-#tmax, dt = 10, 0.01
-#t = np.arange(0, tmax)
-#zac_pog = np.array([np.pi/2, 0, 3*np.pi/4, 0])
-#
-#rk = odeint(sistem_num, zac_pog, t, args=(l1, l2, m1, m2))
+# numerična integracija
+tmax, dt = 10, 0.01
+t = np.arange(0, tmax, dt)
+zac_pog = np.array([np.pi/2, 0, 3*np.pi/4, 0])
 
+rk = odeint(sistem_num, zac_pog, t, args=(l1, l2, m1, m2, g))
+
+print("Integracija uspešno zaključena. Končni kot theta1:", rk[:5])
 
